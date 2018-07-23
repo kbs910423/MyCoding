04b6f6301a3f4c88649a115effaa6cca42226a5e
kwonbaesik <kbs910423@naver.com>
2018-07-23 17:29:47 +0900

    BOJ-SangGeunTrip
    It can be solved BFS or MST. But I use simple geometry

--- Main.java
+++ Main.java
@@ -1,81 +1,33 @@
-package Execricise;
+package SangGeunTrip;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		//input process
-		 BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
 		 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		 int T = Integer.parseInt(in.readLine());
 		 
-		 	String VE = in.readLine();
-		 	StringTokenizer st = new StringTokenizer(VE); 
-		 	int v = Integer.parseInt(st.nextToken()); 
-			int E = Integer.parseInt(st.nextToken()); 
-			
-			//token
-			int[][] graph = new int[v+1][v+1];
-			for(int i=0; i<E; i++){
+		 for(int i=0; i<T; i++){
+			 String ve = in.readLine();
+			 StringTokenizer VE = new StringTokenizer(ve);
+			 int v = Integer.parseInt(VE.nextToken());
+			 int e = Integer.parseInt(VE.nextToken());
+			 int[][] graph = new int[v+1][v+1];
+			 for(int j=0; j<e; j++){
 				String ved = in.readLine();
 				StringTokenizer abc = new StringTokenizer(ved);
 				int a = Integer.parseInt(abc.nextToken());
 				int b = Integer.parseInt(abc.nextToken());
-				int c = Integer.parseInt(abc.nextToken());		
-				graph[a][b] = c;
-				//if(i==0)
-					//min=graph[a][b];
+				graph[a][b] = 1; 
+				graph[b][a] = 1;
 			}
-			int[][] dist = new int[v+1][v+1];
-			
-			//graph를 dist로 옮기는 과정
-			for(int i=0; i<v+1; i++){
-				for(int j=0; j<v+1; j++){
-					dist[i][j] = graph[i][j];	
-					if(dist[i][j]==0)
-						dist[i][j]=-1;
-					//System.out.print(dist[i][j]);
-					}
-				//System.out.println(" ");
-			}
-			//시작 flod & washall?
-			for(int i=0; i<v+1; i++){//거처가는 vertex
-				for(int x=0; x<v+1; x++){//출발하는 시작점
-					for(int y=0; y<v+1; y++){//끝나는 vertex
-						if(dist[x][i] == -1 || dist[i][y] ==-1  || dist[x][y] == -1)
-							continue;
-						if(dist[x][i] + dist[i][y] < dist[x][y])
-							dist[x][y] = dist[x][i]+dist[i][y];
-					}
-				}
-			}			
-			int visit=0;
-			int min=0;
-			for(int i=0; i<v+1; i++){
-				for(int j=0; j<v+1; j++){
-					if((dist[i][j] >= 0) && (dist[j][i] >= 0)){
-						if(visit==0){//miniumum value initialize
-							min=dist[i][j]+dist[j][i];
-							visit++;
-						}
-						if((dist[i][j] + dist[j][i]) < min)
-							min = dist[i][j] + dist[j][i];
-					}
-				
-				}
-			}
-			if(min==0)
-				System.out.println("-1");
-			
-			else
-				System.out.println(min);
+			 System.out.println(v-1);
+		 }
+			 
 	}
-
 }
+
